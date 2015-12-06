03ebdb6a030d73b9e754e5dfde826212cdfbc693
Jake Garrison <omonoid@uw.edu>
2015-12-06 14:59:28 -0800

    some edge work

--- c_filter.m
+++ c_filter.m
@@ -2,7 +2,7 @@ close all
 
 
 %% Inputs
-I = imread('images/flower.jpg');
+I = imread('images/suit.jpg');
 [n,m,dim] = size(I);
 areaMag = numel(num2str(n*m));
 sharpness = 1.5;    % criteria for gradient (1.01 to 2)
@@ -57,8 +57,10 @@ figure; imshow(S); title('Smooth')
 % subplot(2,4,i+1); imshow(S); title(['Smoothing Coeff : ' num2str(sm_constants(i))])
 % end
 
+% high = highboost_filter(S, 5, 1);
+% imshow(high)
 %% Edge detection
-scale = n/1000 % used to work with different sizes
+scale = n/1000; % used to work with different sizes
 E = edge(rgb2gray(I), 'Sobel');
 BW = bwareaopen(E , round(minArea * scale)); % remove white noise < minArea
 se = strel('disk',round(thickness * scale));        
@@ -66,16 +68,18 @@ erode = double(imerode(~BW, se));
 se2 = strel('line',round(9* scale), 90);
 dilate = imdilate(erode,se2);
 figure; imshow(dilate); title('Edge')
-hsv = rgb2hsv(S); 
 
-
+% [Gmag, Gdir] = imgradient(im2bw(I),'CentralDifference');
+% Gmag = double(1-Gmag); imshow(~Gmag);
 
 % Modify Colors
 for i = 1:3
     S(:, :, i) = S(:, :, i).*dilate; % apply weight
 end
 
-figure; imshow(S); title('rgb');
+% Quantize color
+[S,map]= rgb2ind(S,16,'nodither');
+figure, imshow(S,map);
 
 
 
